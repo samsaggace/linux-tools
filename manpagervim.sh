933b5d29b0d5dda8e5f697bec4cdd578b1dd6919
Sebastien Chemin <sebastien.chemin@scmfrance.fr>
2015-10-21 17:42:13 +0200

    Set MANPAGER manually (WHY ?)

--- manpagervim.sh
+++ manpagervim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash --norc
 
 ManpageTitle=$(ps -p $(ps -p $$ -o ppid=) -o args= | awk '{print $NF}')
-ManpageTitle="MANPAGE\ :\ $ManpageTitle"
+ManpageTitle="MANPAGE\ :\ $MAN_PN"
 
-col -b | gvim -R -c -geometry 120x65 "set ft=man nomod nolist titlestring=$ManpageTitle" - > /dev/null
+col -b | MANPAGER='toto' gvim -geometry 120x65 -R -c  "set ft=man nomod noma nolist titlestring=$ManpageTitle" -c "let \$MANPAGER='$MANPAGER'" - > /dev/null
 
