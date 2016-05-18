27c44364fe67c2dc5d69b776b77321435159738e
Sebastien Chemin <sebastien.chemin@scmfrance.fr>
2016-05-18 14:39:58 +0200

    Add new packages and indent properly

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#To run in root 
+#To run in root
 # Docker :
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" >> /etc/apt/sources.list.d/docker.list
@@ -14,5 +14,22 @@ add-apt-repository ppa:git-core/ppa
 
 apt-get update
 
-apt-get install cifs-utils network-manager-openvpn network-manager-openvpn-gnome konsole kdiff3 compizconfig-settings-manager compiz-plugins-extra
-vim-gnome openvpn network-manager-openvpn docker-engine google-chrome-beta zsh git gitk php
+apt-get install \
+    cifs-utils \
+    network-manager-openvpn \
+    network-manager-openvpn-gnome \
+    konsole \
+    kdiff3 \
+    compizconfig-settings-manager \
+    compiz-plugins-extra \
+    vim-gnome \
+    openvpn \
+    network-manager-openvpn \
+    docker-engine \
+    google-chrome-beta \
+    zsh \
+    git \
+    gitk \
+    php \
+    php-fdomdocument \
+    aptitude
